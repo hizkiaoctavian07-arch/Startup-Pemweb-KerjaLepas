03816424c33ac5d2431b6b1cbbed24ffa2304312
hizkiaoctavian07-arch <hizkiaoctavian07@gmail.com>
2026-07-01 15:11:19 +0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { verifikasiToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
